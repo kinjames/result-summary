53719efb79def165ccd10b75d2b2c88cb854f7df
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-05-01 19:38:56 +0000

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Container = () => {
+  return (
+    <section className="container">
+      <CardOne />
+      <CardTwo />
+    </section>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const CardOne = () => {
+  return (
+    <div className="card-one">
+      <p>Your Result</p>
+      <div className="score">
+        <h1>76</h1>
+        <p className="total">of 100</p>
+      </div>
+      <h2>Great</h2>
+      <p className="text">
+        You scored higher than 65% of the people who have taken these tests
+      </p>
+    </div>
+  );
+};
+
+const CardTwo = () => {
+  return (
+    <div className="cardTwo">
+      <Items course={"Reaction"} />
+      <Items course={"Memory"} />
+      <Items course={"Verbal"} />
+      <Items course={"Visual"} />
+    </div>
+  );
+};
+
+const Items = (props) => {
+  return (
+    <div className="items">
+      <div className="image-text">
+        <img src={props.image} alt={props.alternate} />
+        <span className="course">{props.course}</span>
+      </div>
+      <div className="scores">
+        <span className="mainScore">{props.score}</span>
+        <span className="percentile">{props.percent}</span>
+      </div>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Container />);
